a4db1dc886d8d1ddf14723e8ffd3d6fc629d3d76
Timo Heister <timo.heister@gmail.com>
2016-03-17 16:00:45 +0100

    make table of contents

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 for dir in $( ls | grep lab ); do
-  echo $dir/$dir.pdf
+  echo "$dir/$dir.pdf:"
   cd $dir
+  grep "#  Lab " README.md
   rm -f *pdf
   pandoc README.md -V geometry:margin=1in -o $dir.pdf
   cd ..
